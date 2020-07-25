6c20abde23117cb1c8f5eaec09834108dea4e4f9
Dan Hunt <daniel.james.hunt@cern.ch>
2020-07-25 22:15:38 +0200

    AnalysisManager

---
+++ include/B1Analysis.hh
@@ -0,0 +1,7 @@
+#ifndef B1Analysis_h
+#define B1Analysis_h 1
+ 
+#include "g4root.hh"
+//#include "g4xml.hh"
+//#include "g4csv.hh"
+#endif
--- src/B1RunAction.cc
+++ src/B1RunAction.cc
@@ -39,12 +39,24 @@
 #include "G4LogicalVolume.hh"
 #include "G4UnitsTable.hh"
 #include "G4SystemOfUnits.hh"
+#include "B1Analysis.hh"
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 B1RunAction::B1RunAction()
 : G4UserRunAction()
-{}
+{
+	auto analysisManager = G4AnalysisManager::Instance();
+	G4cout << "Using" << analysisManager->GetType() << G4endl;
+
+	// Create nTuple
+	analysisManager->CreateNtuple("Results", "Properties of interest");
+	analysisManager->CreateNtupleDColumn("Edep"); // ID = 0
+	analysisManager->CreateNtupleDColumn("Energy"); // ID = 1
+	analysisManager->CreateNtupleDColumn("Mass"); // ID = 2
+	analysisManager->CreateNTupleSColumn("VolName"); // ID = 3
+	analysisManager->CreateNTupleDColumn("Pos"); // ID = 4
+}
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
